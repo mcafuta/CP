5807b47e4f0d34c58bdbf3f239ec326e11b609a3
mcafuta <mcafuta@users.noreply.github.com>
2017-10-03 01:09:10 +0200

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,13 @@
+# Rename to auth.py
+# This sample data should be enough to test the parts of the site
+# which do not require a database connection
+
+dbase = 'database'
+dbuser = 'username'
+dbpass = 'password'
+dbhost = 'localhost'
+
+# For Python 3, gitkey must be bytes
+gitkey = b'github-secret'
+sesskey = 'session-secret'
+debug = True
